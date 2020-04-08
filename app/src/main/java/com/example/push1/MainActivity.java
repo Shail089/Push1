b6e063fe1718fc64d5eda28c4040f48d23d37e19
Shail089 <verma.shailendra725@gmail.com>
2020-04-08 10:19:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/push1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.push1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void functionAdded(){
+        int a=1;
+        int b=2;
+    }
+}
